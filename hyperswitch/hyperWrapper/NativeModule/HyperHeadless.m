88a7f72815801b00abc7c764a8c8d15ab138dc34
harshsriv <harshit.srivastava@juspay.in>
2024-06-19 08:04:38 +0530

    fix: minor type fix

--- hyperswitch/hyperWrapper/NativeModule/HyperHeadless.m
+++ hyperswitch/hyperWrapper/NativeModule/HyperHeadless.m
@@ -11,6 +11,6 @@
 
 RCT_EXTERN_METHOD(initialisePaymentSession: (RCTResponseSenderBlock)rnCallback)
 RCT_EXTERN_METHOD(getPaymentSession: (NSDictionary)rnMessage :(NSDictionary)rnMessage2 :(NSArray)rnMessage3 :(RCTResponseSenderBlock)rnCallback)
-RCT_EXTERN_METHOD(exitHeadless: (NSDictionary)rnMessage)
+RCT_EXTERN_METHOD(exitHeadless: (NSString)rnMessage)
 
 @end
