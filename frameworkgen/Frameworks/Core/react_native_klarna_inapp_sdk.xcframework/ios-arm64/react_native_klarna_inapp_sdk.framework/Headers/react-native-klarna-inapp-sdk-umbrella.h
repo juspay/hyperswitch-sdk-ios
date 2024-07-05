aeaf1f57cf25c49977cab00d62ecb046f37f00fb
harshsriv <harshit.srivastava@juspay.in>
2024-07-05 22:44:18 +0530

    Continuous Integration

---
+++ frameworkgen/Frameworks/Core/react_native_klarna_inapp_sdk.xcframework/ios-arm64/react_native_klarna_inapp_sdk.framework/Headers/react-native-klarna-inapp-sdk-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "KlarnaPaymentView.h"
+#import "PaymentViewWrapper.h"
+
+FOUNDATION_EXPORT double react_native_klarna_inapp_sdkVersionNumber;
+FOUNDATION_EXPORT const unsigned char react_native_klarna_inapp_sdkVersionString[];
+
