917786a6ee8227efe06f057b0b683401021960c1
25harsh <harshit.srivastava@juspay.in>
2024-10-25 17:57:14 +0530

    Release 0.1.6

--- hyperswitch/hyperWrapper/NativeModule/HyperHeadless.swift
+++ hyperswitch/hyperWrapper/NativeModule/HyperHeadless.swift
@@ -47,7 +47,7 @@ internal class HyperHeadless: RCTEventEmitter {
             if PaymentSession.headlessCompletion != nil, !PaymentSession.isPresented {
                 let hyperParams = [
                     "appId": Bundle.main.bundleIdentifier,
-                    "sdkVersion" : "0.1.6.patch1",
+                    "sdkVersion" : "0.1.6",
                     "ip": nil,
                     "user-agent": WKWebView().value(forKey: "userAgent"),
                     "launchTime": Int(Date().timeIntervalSince1970 * 1000)
