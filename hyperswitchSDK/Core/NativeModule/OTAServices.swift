24980cde2d230592bbf26fdbdd1158ba134c5ba8
Kuntimaddi Manideep <manideepk90@gmail.com>
2025-01-27 15:09:45 +0530

    removed try catch block

--- hyperswitchSDK/Core/NativeModule/OTAServices.swift
+++ hyperswitchSDK/Core/NativeModule/OTAServices.swift
@@ -21,22 +21,16 @@ class MyLogger: NSObject, HPJPLoggerDelegate {
 }
 
 public final class OTAServices {
-    public static func getBundleURL() -> URL? {
+    public static func getBundleURL() -> URL {
         let payload = [
             "clientId": "hyperswitch",
             "namespace": "hyperswitch", // Default is juspay.
             "forceUpdate": true,
             "localAssets": false, // To use assets from the app's bundle. Default is false.
             "fileName": "hyperswitch.bundle", // Index file name. Default is 'main.jsbundle'.
-            "releaseConfigURL": "http://10.10.68.233:3000/files/hyperswitch/ios/release-config.json", // demo ServerURL
+            "releaseConfigURL": "http://localhost:3000/files/hyperswitch/ios/release-config.json", // demo ServerURL
         ] as [String: Any]
         let logger = MyLogger()
-        
-        do {
-            let bundleURL = try HyperOTAServices.bundleURL(payload, loggerDelegate: logger)
-            return bundleURL
-        } catch {
-            return Bundle.main.url(forResource: "hyperswitch", withExtension: "bundle")
-        }
+        return HyperOTAServices.bundleURL(payload, loggerDelegate: logger)
     }
 }
