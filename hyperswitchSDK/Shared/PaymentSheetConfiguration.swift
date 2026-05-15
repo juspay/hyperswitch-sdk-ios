b886d72a698c15946000a427d7ec55fb2f192b05
Shivam <shivam.nan@juspay.in>
2026-05-15 12:32:37 +0530

    feat: add alwaysSendCustomerAcceptance prop

--- hyperswitchSDK/Shared/PaymentSheetConfiguration.swift
+++ hyperswitchSDK/Shared/PaymentSheetConfiguration.swift
@@ -64,6 +64,10 @@ extension PaymentSheet {
         public var displaySavedPaymentMethods: Bool? = true
 
         ///
+        /// toggle to always send customer acceptance
+        public var alwaysSendCustomerAcceptance: Bool? = false
+
+        ///
         /// toggle to disable Branding
         public var disableBranding: Bool? = false
 
