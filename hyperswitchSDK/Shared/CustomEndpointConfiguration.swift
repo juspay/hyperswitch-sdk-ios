9ef5be3f3ebee812af5c2816d8a40f7589e88128
Harsh <128193838+25harsh@users.noreply.github.com>
2026-05-18 16:52:54 +0530

    feat: added environment, fixed customconfig (#97)

--- hyperswitchSDK/Shared/CustomEndpointConfiguration.swift
+++ hyperswitchSDK/Shared/CustomEndpointConfiguration.swift
@@ -6,8 +6,40 @@
 //
 
 public enum CustomEndpointConfiguration: Codable {
-    case customEndpoint(String)
+    case commonEndpoint(String)
     case overrideEndpoints(OverrideEndpointConfiguration)
+
+    private enum CodingKeys: String, CodingKey {
+        case customEndpoint
+        case overrideEndpoints
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .commonEndpoint(let value):
+            try container.encode(value, forKey: .customEndpoint)
+        case .overrideEndpoints(let config):
+            try container.encode(config, forKey: .overrideEndpoints)
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let value = try container.decodeIfPresent(String.self, forKey: .customEndpoint) {
+            self = .commonEndpoint(value)
+        } else if let config = try container.decodeIfPresent(OverrideEndpointConfiguration.self, forKey: .overrideEndpoints) {
+            self = .overrideEndpoints(config)
+        }
+        else {
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(
+                    codingPath: decoder.codingPath,
+                    debugDescription: ""
+                )
+            )
+        }
+    }
 }
 
 public struct OverrideEndpointConfiguration: Codable {
